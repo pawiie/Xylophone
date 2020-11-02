21ef76cf196392d5420241687944a5fdfc09fbc1
pawiie <pawan5653989@gmail.com>
2020-11-02 14:36:30 +0530

    added assets and icons

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Xylophone(),
+    ),
+  ));
+}
+
+class Xylophone extends StatefulWidget {
+  @override
+  _XylophoneState createState() => _XylophoneState();
+}
+
+class _XylophoneState extends State<Xylophone> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
