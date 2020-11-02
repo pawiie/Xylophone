80f09ac6c2a86b8b8fce365e1c1d69cb877f8cc3
pawiie <pawan5653989@gmail.com>
2020-11-02 16:12:32 +0530

    added functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,8 +15,80 @@ class Xylophone extends StatefulWidget {
 }
 
 class _XylophoneState extends State<Xylophone> {
+  void play(int a) {
+    final player = AudioCache();
+    player.play('note$a.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              play(1);
+            },
+            color: Colors.deepPurple,
+            // height: 100,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              play(2);
+            },
+            color: Colors.indigo,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              play(3);
+            },
+            color: Colors.blue,
+            // height: 100,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              play(4);
+            },
+            color: Colors.green,
+            // height: 100,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              play(5);
+            },
+            color: Colors.yellow,
+            // height: 100,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              play(6);
+            },
+            color: Colors.orange,
+            // height: 100,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              play(7);
+            },
+            color: Colors.red,
+            // height: 100,
+          ),
+        ),
+      ],
+    );
   }
 }
