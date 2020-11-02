31f662d55f7b49ab42c05b6ab5c305399d4ed3a2
pawiie <pawan5653989@gmail.com>
2020-11-02 15:07:46 +0530

    added audio player

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Xylophone(),
+      body: SafeArea(child: Xylophone()),
     ),
   ));
 }
